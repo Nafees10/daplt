271ce5ca1353bff3f925f494be66d65da6c10e16
Nafees Hassan <nafeeshassan16@gmail.com>
2023-08-16 20:30:39 +0500

    Added test println function

--- source/test.d
+++ source/test.d
@@ -1,12 +1,23 @@
 module test;
 
 import plutonium;
-import std.stdio;
+import std.stdio,
+			 std.string;
 
 version (test){
 	extern (C) PltObject init(){
 		writeln("init called");
 		auto mod = allocModule("test");
+		addModuleMember(mod, "println\0", PObjFromNativeFun(
+					"println\0", &println));
+
 		return mod;
+	}
+
+	extern (C) PltObject println(PltObject* argv, int argc){
+		foreach (i; 0 .. argc)
+			write(fromStringz(strAsCstr(argv[i])));
+		writeln();
+		return nil;
 	}
 }
