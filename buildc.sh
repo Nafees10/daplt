9f6e7efe293e532020c39b6fe1de31eb3a10bae0
Nafees Hassan <nafeeshassan16@gmail.com>
2023-09-15 03:49:13 +0500

    Bug fixes in building c_api

--- buildc.sh
+++ buildc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ ! -f c_api.o ]; then
-	echo "building c_api"
-	g++ -c plt/c_api.cpp -fPIC -lstdc++ -o c_api.o
+if [ ! -f "$1/c_api.o" ]; then
+	echo "building c_api to $1"
+	g++ -c "$1/plt/c_api.cpp" -fPIC -lstdc++ -o "$1/c_api.o"
 fi
